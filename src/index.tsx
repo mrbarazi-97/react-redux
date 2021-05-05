513179ed796aa7634d76ca29663a5485d93a3934
mrbarazi-97 <mrbarazi.97@gmail.com>
2021-05-06 00:32:22 +0430

    first-tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return <div>
+        <h1>Hi there!</h1>
+    </div>
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
