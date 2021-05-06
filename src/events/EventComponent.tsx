1fa51282fd3c8dfb952c624bcc1317613c7cf157
mrbarazi-97 <mrbarazi.97@gmail.com>
2021-05-07 04:06:20 +0430

    Events

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EventComponent: React.FC = () => {
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event);
+    };
+
+    const onDragStart = (event: React.DragEvent<HTMLInputElement>) => {
+        console.log(event);
+    };
+
+    return (
+        <div>
+            <input onChange={onChange} />
+            <div draggable onDragStart={onDragStart}>Drag me!</div>
+        </div>);
+};
+
+
+export default EventComponent;
